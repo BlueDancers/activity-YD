6325505bdd1ef540946f8da40ea760d422dfcb3f
小五 <wu.vkcyan@gmail.com>
2020-02-09 19:05:28 +0800

    修改 publicPath

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
 const path = require('path')
 module.exports = {
   //关闭eslint
-  publicPath: "./",
+  publicPath: "/htdocs/",
   lintOnSave: false,
   productionSourceMap: false,
   css: {
