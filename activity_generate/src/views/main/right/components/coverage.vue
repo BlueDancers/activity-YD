268c8d307404fce021bbe85e9aebc015648eef67
lgldlk <1851293758@qq.com>
2020-05-16 13:24:18 +0800

    图层的更改

--- activity_generate/src/views/main/right/components/coverage.vue
+++ activity_generate/src/views/main/right/components/coverage.vue
@@ -2,52 +2,37 @@
 import templateVue from '@/views/main/left/components/template.vue';
  * @Author: your name
  * @Date: 2020-05-08 14:12:40
- * @LastEditTime: 2020-05-12 15:55:37
+ * @LastEditTime: 2020-05-16 13:20:58
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \activity_generate\src\views\main\right\components\coverage.vue
  -->
 <template>
-<div class='coverage'>
-  
-  <div  class="list">
-      <draggable
-      class="syllable_ul"
-            element="ul"
-            :list="templateLists"
-            :options="{group:'title', animation:150}"
-            :no-transition-on-drag="true"
-            @change="change"
-            @start="start"
-            @end="end"
-            :move="move"
-      >
-      <transition-group type="transition" :name="!drag? 'fade' : null" :css="true">
-        <li v-for="(item,index) in templateLists" :key="'cell'+index"  @click="addItem(item.activityId)"
-        :class="{'cells':true ,'activeCells':isActive(item.activityId)  }" 
-
-        >
-        {{item.name}}
-        </li>
-      </transition-group>
-       </draggable>
+  <div class='coverage'>
+    <div class="list">
+      <draggable class="syllable_ul" element="ul" :list="templateLists" :options="{group:'title', animation:150}" :no-transition-on-drag="true" @change="change" @start="start" @end="end" :move="move">
+        <transition-group type="transition" :name="!drag? 'fade' : null" :css="true">
+          <li v-for="(item,index) in templateLists" :key="'cell'+index" @click="addItem(item.activityId)" 
+          :class="{'cells':true ,'activeCells':isActive(item.activityId) }">
+            <p>{{item.name}}</p>
+          </li>
+        </transition-group>
+      </draggable>
     </div>
-</div>
+  </div>
 </template>
 <script>
 import draggable from "vuedraggable"
 export default {
-    components:{
-      draggable,
-      
+  components:{
+    draggable,
   },
   props:[],
   mounted(){
-    
   },
     data() {
     return {
-      cache:{
+      cache:{//以存储的类型
           keyValue:{
             namekey:[],
             value:[]
@@ -61,13 +46,10 @@ export default {
       changeTemplate:{},
       drag:false,
     }
-
   },
 
   computed:{
-    
-    templateLists(){
-        
+    templateLists(){//让每个图层拖动后的名字都不变，及每个ID对应唯一的名字
        let tmp=JSON.parse(JSON.stringify(this.$store.state.core.template));
          tmp.sort(
            (a,b)=>{
@@ -76,14 +58,10 @@ export default {
                );
       tmp.map(
         (item)=>{
-         
-
           if(!this.cache.keyValue.namekey.includes(item.activityId)){//没存储的对象
-          
           if(!this.cache.nameCache.namekey.includes(item.name)){//新的类型
               this.cache.nameCache.namekey.push(item.name);
               this.cache.nameCache.value.push(1);
-              
             }
             this.cache.keyValue.namekey.push(item.activityId);
             this.cache.keyValue.value[this.cache.keyValue.namekey.indexOf(item.activityId)]=this.getTemplateName(item.name)
@@ -95,7 +73,6 @@ export default {
           }
           item.name=this.cache.keyValue.value[this.cache.keyValue.namekey.indexOf(item.activityId)]
         }
-       
       );
       tmp.sort(
         (a,b)=>{
@@ -107,14 +84,12 @@ export default {
     activeTemplates(){
       return this.$store.state.core.activeTemplate;
     }
-
   },
-  
   watch:{
   },
-  
+
   methods: {
-    getTemplateName(name){
+    getTemplateName(name){//获取对应的名字
       switch(name){
         case 'base-div':
           return '块级元素';
@@ -128,22 +103,13 @@ export default {
           return '输入框';
         case 'base-swiper':
           return '轮播图';
-        
       }
-
     },
-
-     //evt里面有两个值，一个evt.added 和evt.removed  可以分别知道移动元素的ID和删除元素的ID
     change(e) {
-      
-      //console.log(e , 'change...')
     },
-    //start ,end ,add,update, sort, remove 得到的都差不多
     start(e) {
       this.drag = true;
       this.startTemplate=e.item._underlying_vm_;
-     // console.log(this.startTemplate);
-     // console.log(e , 'start...')
     },
     end(e) {
       //console.log(e , 'end....')
@@ -158,39 +124,26 @@ export default {
         this.setZIndex(this.changeTemplate.activityId,++change);
         this.setZIndex(this.startTemplate.activityId,change-1);
       }
-      console.log(this.startTemplate.css.zIndex,this.changeTemplate.css.zIndex);
-
-      // evt.item //可以知道拖动的本身
-      // evt.to    // 可以知道拖动的目标列表
-      // evt.from  // 可以知道之前的列表
-      // evt.oldIndex  // 可以知道拖动前的位置
-      // evt.newIndex  // 可以知道拖动后的位置
     },
 
     move(e, originalEvent) {
       this.changeTemplate=e.relatedContext.element;
-      console.log(this.changeTemplate);
-      //console.log(e , 'move')
-      //console.log(originalEvent,"鼠标位置") //鼠标位置
     },
     isActive(id){
     return this.activeTemplates.includes(id);
     },
     setZIndex(id,index){
-    for(let i=0,len= this.$store.state.core.template.length;i<len;i++){
-       if(this.$store.state.core.template[i].activityId==id){
+      for(let i=0,len= this.$store.state.core.template.length;i<len;i++){
+        if(this.$store.state.core.template[i].activityId==id){
           this.$store.state.core.template[i].css.zIndex=index;
            break;
         }
-                    
-       }
+      }
     },
     addItem(itemId){
-           
           this.$store.commit("core/toggle_temp_status",itemId);
     },
-    dragEnd (e, item) {
-
+    dragEnd (e, item) {//交换图层的zIndex
       let change=this.changeTemplate.css.zIndex,
           start=this.startTemplate.css.zIndex;
       if(change>start){
@@ -200,11 +153,6 @@ export default {
         this.setZIndex(this.changeTemplate.activityId,++change);
         this.setZIndex(this.startTemplate.activityId,change-1);
       }
-    
-      // e.preventDefault();
-      /**
-       * 进行拖拽完成的操作
-       */
     },
   },
 }
@@ -218,34 +166,36 @@ export default {
   margin: 0;
   overflow-y:auto;
   height: 84vh;
-  border-top: 1px #eee solid;
+  border-top: 1px rgb(192, 192, 192)  solid;
+  border-bottom: 2px rgb(192, 192, 192)  solid;
   .list{
-      
-      
+    cursor:n-resize s-resize;
     .cells{
-      height: 2em;
-      line-height: 2em;
-      font-size: 1.5em;
+      height: 4em;
+      line-height: 4em;
+      font-size: 1em;
       position: relative;
-      padding-left: 1em;
-      outline: 1px #eee solid;
+      cursor: move;
+      vertical-align:top;
+      padding-left:3em;
+      outline: 1px rgb(192, 192, 192) solid;
+      p{
+        cursor: pointer;
+      }
     }
-    
   }
 }
 
 .activeCells{
-      
       &::before{
         content: '';
-        background-color: skyblue;
-        opacity: 0.2;
+        background-color: #4190f7;
+        opacity: 0.3;
         position:absolute;
-            top: 0;
+        top: 0;
         right: 0;
         bottom: 0;
        left: 0;
-       
       }
     }
     .fade-enter-active, .fade-leave-active {
